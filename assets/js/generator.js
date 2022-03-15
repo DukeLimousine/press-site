0ead0142b20c98781af0864966b40c82beacdf6f
Louis Di Domenico <lou.didomenico@swiftkickweb.com>
2022-03-15 19:45:30 -0400

    more generator fixes

--- assets/js/generator.js
+++ assets/js/generator.js
@@ -1,41 +1,56 @@
 var site = site || {};
 
-document.addEventListener("DOMContentLoaded", function() {
-  let generatorForm = document.getElementById('generator-form');
+let generator = (() => {
   let titles = [];
   let firstNames = [];
   let lastNames = [];
+  let generatorForm = document.getElementById('generator-form');
+  let resultArea = generatorForm && generatorForm.querySelector('.js-generator-result');
 
-  function processData(arr, val) {
+  let dataLoop = () => {
+    if (site.titles) {
+      for (const key in site.titles ) {
+        processData(titles, site.titles[key]);
+      };
+    }
+
+    if (site.firstNames) {
+      for (const key in site.firstNames ) {
+        processData(firstNames, site.firstNames[key]);
+      };
+    }
+
+    if (site.lastNames) {
+      for (const key in site.lastNames ) {
+        processData(lastNames, site.lastNames[key]);
+      };
+    }
+  }
+
+  let processData = (arr, val) => {
     arr.push(val.split('=>')[1].split(',')[0].replace(/"|}/g,""));
   }
 
-  if (site.titles) {
-    for (const key in site.titles ) {
-      processData(titles, site.titles[key]);
-    };
+  let generatorListener = () => {
+    generatorForm.addEventListener('submit', function(e) {
+      e.preventDefault();
+      let title = titles[Math.floor(Math.random() * titles.length)];
+      let firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+      let lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+      let fullName = title + " " + firstName + " " + lastName;
+      resultArea.innerHTML = fullName;
+    });
   }
 
-  if (site.firstNames) {
-    for (const key in site.firstNames ) {
-      processData(firstNames, site.firstNames[key]);
-    };
-  }
+  return {
+    init: () => {
+      dataLoop();
+      generatorListener();
+    }
+  };
 
-  if (site.lastNames) {
-    for (const key in site.lastNames ) {
-      processData(lastNames, site.lastNames[key]);
-    };
-  }
+})();
 
-  generatorForm.addEventListener('submit', function(e) {
-    e.preventDefault();
-    let title = titles[Math.floor(Math.random() * titles.length)];
-    let firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
-    let lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
-    let fullName = title + " " + firstName + " " + lastName;
-
-    generatorForm.querySelector('.js-generator-result').innerHTML = fullName;
-  });
-
+document.addEventListener("DOMContentLoaded", function() {
+  generator.init();
 });
