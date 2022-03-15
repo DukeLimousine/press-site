2cbcf3bee253ca0d55cdfacd32c787fd6b6a215f
Louis Di Domenico <lou.didomenico@swiftkickweb.com>
2022-03-15 15:36:29 -0400

    undoing all that csv bullshit

--- assets/js/generator.js
+++ assets/js/generator.js
@@ -1,95 +1,34 @@
-// Homebrew Javascript CSV reader. Source data in /generator_data/
-let generator = (() => {
+---
+---
+
+document.addEventListener("DOMContentLoaded", function() {
   let generatorForm = document.getElementById('generator-form');
   let titles = [];
   let firstNames = [];
   let lastNames = [];
-  let dataSourceUrls = ['titles.csv','first_names.csv','last_names.csv'];
-  let generatorDataLoc = 'generator_data/';
-  
-  // Thanks to https://stackoverflow.com/questions/5933565/how-to-create-initialize-the-file-object-using-file-path-html5
-  let getFileBlob = function (url, cb) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.responseType = "blob";
-    xhr.addEventListener('load', function() {
-        cb(xhr.response);
-    });
-    xhr.send();
-  };
 
-  let blobToFile = function (blob, name) {
-    blob.lastModifiedDate = new Date();
-    blob.name = name;
-    return blob;
-  };
-
-  let getFileObject = function(filePathOrUrl, filename, cb) {
-    getFileBlob(filePathOrUrl, function (blob) {
-      cb(blobToFile(blob, filename));
-    });
-  };
-
-  let processData = (data, dataFileName) => {
-    let lbreak = data.split("\n");
-    let resultArray;
-
-    switch (dataFileName) {
-      case 'titles.csv':
-        resultArray = titles;
-        break;
-      case 'first_names.csv':
-        resultArray = firstNames;
-        break;
-      case 'last_names.csv':
-        resultArray = lastNames;
-        break;
-      default:
-        console.log('File not found');
-    }
-
-    lbreak.forEach(res => {
-      resultArray.push(res.split(",")[0]);
-    });
-
-    resultArray = resultArray.shift(); //remove csv header. This might be kind of hacky.
+  function processData(arr, val) {
+    arr.push(val.split('=>')[1].split(',')[0].replace(/"/g,""));
   }
 
-  let dataLoop = () => {
-    dataSourceUrls.forEach((dataFileName) => {
-      let reader = new FileReader();
-      getFileObject(generatorDataLoc + dataFileName, dataFileName, function (fileObject) {
-         reader.readAsBinaryString(fileObject);
-         reader.onload = (e) => {
-           let fileData = e.target.result;
-           processData(fileData, dataFileName);
-         }
-      }); 
+  {% for value in site.data.titles %}
+    processData(titles, '{{value}}' );
+  {% endfor %}
+  {% for value in site.data.first_names %}
+    processData(firstNames, '{{value}}' );
+  {% endfor %}
+  {% for value in site.data.last_names %}
+    processData(lastNames, '{{value}}' );
+  {% endfor %}
 
-    });
-  }
+  generatorForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    let title = titles[Math.floor(Math.random() * titles.length)];
+    let firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
+    let lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
+    let fullName = title + " " + firstName + " " + lastName;
 
-  let generatorListener = () => {
-      generatorForm.addEventListener('submit', function(e) {
-      e.preventDefault();
-      let title = titles[Math.floor(Math.random() * titles.length)];
-      let firstName = firstNames[Math.floor(Math.random() * firstNames.length)];
-      let lastName = lastNames[Math.floor(Math.random() * lastNames.length)];
-      let fullName = title + " " + firstName + " " + lastName;
+    generatorForm.querySelector('.js-generator-result').innerHTML = fullName;
+  });
 
-      generatorForm.querySelector('.js-generator-result').innerHTML = fullName;
-    });
-  }
-
-  return {
-    init: () => {
-      dataLoop();
-      generatorListener();
-    }
-  };
-
-})();
-
-document.addEventListener("DOMContentLoaded", function() {
-  generator.init();
 });
